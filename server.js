690a9d07204950d76d7b04c122b4882d44c0f145
Rahul <rahulthokal90@gmail.com>
2019-08-04 19:50:13 +0530

    fdsgdfgd

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ connectDB();
 app.use(express.json({ extended: false }));
 //app.use(cors());
 // Define Routes
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
